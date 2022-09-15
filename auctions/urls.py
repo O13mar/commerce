1a55c954605d37deda4db15bc2f7d1aa28b5c938
O13mar <omaralsharef353@gmail.com>
2022-09-15 18:50:54 +0300

    'co'

---
+++ auctions/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create",views.createListings,name="create"),
+    path("displayCategory",views.displayCategory,name="displayCategory"),
+    path("listing/<int:id>",views.listing,name="listing"),
+    path("removeWatchlist/<int:id>",views.removeWatchlist,name="removeWatchlist"),
+    path("addWatchlist/<int:id>",views.addWatchlist,name="addWatchlist"),
+    path("watchlist",views.watchlist,name="watchlist"),
+    path("addcomment/<int:id>",views.addcomment,name="addcomment"),
+    path("addBid/<int:id>",views.addBid,name="addBid"),
+    path("closeAuction/<int:id>",views.closeAuction,name="closeAuction")
+]
